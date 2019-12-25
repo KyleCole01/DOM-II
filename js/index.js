1b38d78b178c913a79b2cafa7bd85ee52a8ebe9b
KyleCole01 <kyle@misfitselectronics.com>
2019-12-25 12:19:10 -0500

    added 4 event listeners so far

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+ let navLink = document.querySelectorAll('nav a')[0];
+ let navLink2 = document.querySelectorAll('nav a')[1];
+ let heading = document.querySelector('.logo-heading');
+    
+ 
+ navLink.addEventListener('mouseenter' ,()=>{
+        navLink.style.color = 'green';
+        navLink.style.transform = 'scale(1.5)';
+        navLink.style.transition = 'transform 0.3s'
+    });
+    
+navLink.addEventListener('mouseleave' ,()=>{
+        navLink.style.color = 'black';
+        navLink.style.transform = 'scale(1)';
+        navLink.style.transition = 'transform 0.3s'
+    });
+
+navLink2.addEventListener('mouseover', ()=>{
+    if(navLink2.style.color !== 'purple'){
+        navLink2.style.color = 'purple';
+        navLink2.style.transform = 'scale(1.5)';
+        navLink2.style.transition = 'transform 0.3s'
+    }else{
+        navLink2.style.color = 'black';
+        navLink2.style.transform = 'scale(1)';
+        navLink2.style.transition = 'transform 0.3s'
+    }
+
+});
+heading.addEventListener('dblclick',()=>{
+    if(heading.style.transform !== 'scale(3)'){
+    heading.style.transform = 'scale(3)'
+    }
+    else{
+        heading.style.transform = 'scale(1)'
+    }
+})
